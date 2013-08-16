d40a2f1bc1e48ca041d3ba456c4a9f48509fe7fe
Zach Young <zachjyoung@gmail.com>
2013-08-16 11:28:59 -0400

    installed ruby

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BananaStand::Application.config.secret_key_base = '55cef3c1f751318c9871ef0ee57a59ab5f21b24cc44e64deb81c9feaf2b772da0935aeb981622d6372067ad54d0aa6cb004af42438c20e16ce915ad208b2f75d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BananaStand::Application.config.session_store :cookie_store, key: '_banana_stand_session'
